3488217a1017beb1e95c9af66b5a2e476e1aeccd
fajritaridala <fadjritaridala@gmail.com>
2025-07-30 21:15:26 +0800

    feat: add auth middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,39 @@
 import { NextFunction, Request, Response } from "express";
 import { IReqUser } from "../interfaces/auth.interface";
+import { getUserData } from "../utils/jwt";
 
 export default (req:Request, res:Response, next:NextFunction) => {
-  const tokenJwt = req.headers?.authorization;
+  // Cek apakah ada token di header Authorization
+  const authorization = req.headers?.authorization;
+
+  if (!authorization) {
+    return res.status(403).json({
+      message: "unauthorized",
+      data: null,
+    })
+  }
+
+  // membaca token
+  const [prefix, accessToken] = authorization.split(" "); // @split(" ") untuk memisahkan prefix "Bearer" dan token
+
+  if (!(prefix === "Bearer" && accessToken)) {
+    return res.status(403).json({
+      message: "unauthorized",
+      data: null,
+    });
+  }
+
+  const user = getUserData(accessToken);
+
+  if (!user) {
+    return res.status(403).json({
+      message: "unauthorized",
+      data: null,
+    });
+  }
+
+  // tambahkan user ke request
+  (req as IReqUser).user = user;
+
+  next();
 }
